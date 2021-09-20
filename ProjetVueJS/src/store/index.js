c4de4d4f99fb6a44b398d9a10d7a4dfdd23b4bcf
Camille PERES <camille.peres@free.fr>
2021-09-20 11:32:27 +0200

    Récupérer le username

--- ProjetVueJS/src/store/index.js
+++ ProjetVueJS/src/store/index.js
@@ -2,10 +2,15 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    //data dans Vue
+  },
+  getters: {
+    //computed dans Vue
   },
   mutations: {
   },
   actions: {
+    //methods dans Vue
   },
   modules: {
   }
